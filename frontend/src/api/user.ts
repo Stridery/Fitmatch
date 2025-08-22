f813d8f424a92983956dc5696bce16911db31ca9
Zhenghao You <zhenghaoyou@Mac.attlocal.net>
2025-08-22 12:20:53 -0400

    fixed docker and system build up

--- frontend/src/api/user.ts
+++ frontend/src/api/user.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import type { UserProfile } from '@/entities/user';
 import type { Injury } from '@/entities/user'
 
-const BASE_URL = 'http://localhost:8080/users';  // 你的 user-service 地址
+const BASE_URL = `${import.meta.env.VITE_API_BASE}/users`;  // 你的 user-service 地址
 
 export const setUserProfile = (profile: Partial<UserProfile>, token: string) => {
   return axios.post(`${BASE_URL}/me`, profile, {
@@ -20,7 +20,7 @@ export async function getUserProfile(token: string) {
       },
     });
     return res.data;
-  } catch (err: any) {
+  } catch (err) {
     if (axios.isAxiosError(err) && err.response?.status === 409) {
       // 后端说「找不到 profile」
       return null;
